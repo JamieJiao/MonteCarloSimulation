097f6dd36494c15eda8d36fb857d738e46b98f5f
Jamie Jiao <44076678+JamieJiao@users.noreply.github.com>
2020-07-30 01:12:07 -0400

    Update monte_carlo_simulation.py

--- code/monte_carlo_simulation.py
+++ code/monte_carlo_simulation.py
@@ -27,11 +27,13 @@ def confidence_interval_calculation(data, alpha=0.95):
     lower_ci = mean - st.norm.ppf(alpha)*sigma
     return mean, upper_ci, lower_ci
 
-def simulate_rental(square_feet_generated, coefs_mean, coefs_std_error, simualated_var_name='Area_Size'):
+def simulate_rental(square_feet_generated, coefs_mean, coefs_std_error, simualated_var_name='Square_Feet'):
     """
+    ----------------------------
     Based on regression between reponse variable, Rental, 
     and explanatory variable, SquareFeet,
     to generate simulated Rental.
+    ----------------------------
     """
 
     rental_simulated = 0
@@ -47,25 +49,27 @@ def simulate_rental(square_feet_generated, coefs_mean, coefs_std_error, simualat
 
 def simulate_sold_price(observation_choosen, square_feet_posi, \
                         square_feet_generated, rental_simulated, \
-                        coefs_mean, coefs_std_error, simualated_var_name='Rental_Month'):
+                        coefs_mean, coefs_std_error, simualated_var_name='Rental'):
     """
+    ----------------------------
     Use a specific observation record,
     based on regression, 
     SoldPrice= β_0+ β_1*SquareFeet+ β_2*Rental+ β_3*LotteryDummy ... + ε_i,
     to simulate Sold Price, y.
+    ----------------------------
     """
 
     sold_price_simulated = 0
-    """ convert to numpy array """
+    # convert to numpy array
     observation_choosen = np.array(observation_choosen)
-    """ add constant to the observation """
+    # add constant to the observation
     observation_choosen = np.insert(observation_choosen, 0, 1)
-    """ replace variable by simulated one """
+    # replace variable by simulated one
     observation_choosen[square_feet_posi] = square_feet_generated
 
     for i in range(len(coefs_mean)):
         coef_generated = np.random.normal(coefs_mean[i], coefs_std_error[i], 1)
-        """ exculde Rental coefficient, use simulated Rental instead"""
+        # exculde Rental coefficient, use simulated Rental instead
         if coefs_mean.index[i] != simualated_var_name: 
             term = coef_generated * observation_choosen[i]
         else:
@@ -78,7 +82,12 @@ def sold_price_subsample_generate(observation_choosen, square_feet_generated, \
                                     rental_simulated, full_coefs_mean, full_coefs_std, \
                                     square_feet_posi=1, x_repeat_times=100):
     """
+    ----------------------------
     By repeating X, generate a sample of Y, Sold Price.
+    
+    Parameter:
+    square_feet_posi (the position of the Square Feet variable in the observation),
+    ----------------------------
     """
 
     simulated_sold_price_subsample = []
@@ -95,18 +104,21 @@ def Simulation_Results(observation_choosen, square_feet_mean, square_feet_std, \
                         coefs_mean, coefs_std, \
                         full_coefs_mean, full_coefs_std, sample_size=1000):
     """
+    ----------------------------
     Mean and confidence intervals are generated in each iteration.
 
     Parameters:
-    Observation_choosen (a observation record),
-    square_feet_posi (the position of the Square Feet variable in the observation),
+    Observation_choosen (a observation record);
     ...
-    coefs_mean (coefficients of all variables in the Sold Price regression from statsmodels, 
-    Pandas Series like),
-    coefs_std_error (standard errors of the coefficientsfrom statsmodels, Pandas Dataseries like).
-
+    coefs_mean (coefficients of all variables in the regression between Rental and Square Feet),
+    coefs_std (standard errors of the coefficients in the regression between Rental and Square Feet),
+    full_coefs_mean (coefficients of all variables in the Sold Price regression),
+    full_coefs_std (standard errors of the coefficients in the Sold Price regression),
+    Pandas Series like, from statsmodels.
+    
     Return:
     mean values, lower and upper confidence interval.
+    ----------------------------
     """
 
     simulated_rental_sample = []
@@ -116,13 +128,13 @@ def Simulation_Results(observation_choosen, square_feet_mean, square_feet_std, \
     ci_upper_bound = []
     means = []
     for n in range(sample_size):
-        """ simulate Square Feet """
+        # simulate Square Feet
         square_feet_generated = np.random.normal(square_feet_mean, square_feet_std, 1)
         if square_feet_generated <= 0:
             continue
         else:
             generated_square_feet_sample.append(square_feet_generated[0])
-            """ simulate Rental based on the regression between Rental and Square Feet """
+            # simulate Rental based on the regression between Rental and Square Feet
             rental_simulated = simulate_rental(square_feet_generated, coefs_mean, coefs_std)
             simulated_rental_sample.append(rental_simulated[0])
 
